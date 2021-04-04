f2f7d9a2b83d4b98a0a735743b7379315df958be
bhaskar777 <bhaskarbhowmik777@gmail.com>
2021-04-04 19:25:09 +0530

    ToDo App

--- App.js
+++ App.js
@@ -1,21 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import React from 'react'
+import { View, SafeAreaView } from 'react-native'
+import { actionCreators } from './src/Screens/todoListRedux'
+import List from './src/Screens/List'
+import Input from './src/Screens/Input'
+import Title from './src/Screens/Title'
+import store from './src/Screens/Store'
+export default class App extends React.Component {
+  state = {}
+  unsubscribe = store.subscribe(() => {
+    const {todos} = store.getState()
+    this.setState({todos})
+  }) 
+  componentWillMount() {
+    const {todos} = store.getState()
+    this.setState({todos})
+  }
+  componentWillUnmount() {
+   this.unsubscribe()
+  }
+onAddTodo = (text) => {
+    store.dispatch(actionCreators.add(text))
+  }
+onRemoveTodo = (index) => {
+    store.dispatch(actionCreators.remove(index))
+  }
+render() {
+    const {todos} = this.state
+return (
+      <SafeAreaView>
+        <Title>
+          To-Do List
+        </Title>
+        <Input
+          placeholder={'Type a todo, then hit enter!'}
+          onSubmitEditing={this.onAddTodo}
+        />
+        <List
+          list={todos}
+          onPressItem={this.onRemoveTodo}
+        />
+      </SafeAreaView>
+    )
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
